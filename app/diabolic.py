98b2f9e011263c5757ba5e61c7d59eace8285684
alexsincai <alex.sincai@yahoo.co.uk>
2021-08-27 15:56:01 +0300

    started work on the vowels

--- app/diabolic.py
+++ app/diabolic.py
@@ -1,261 +1,3 @@
-# """
-# Diabolic script generator, and helper functions
-# """
-
-# from typing import List, Optional, Union
-# from re import sub, compile, MULTILINE
-# from PIL import Image
-
-
-# def clean_up_string(string: str) -> str:
-#     """
-#     Removes duplicate consonants, add placeholder for vowels, removes extra spaces
-#     """
-
-
-# def split_into_groups(string: str) -> List[str]:
-#     """
-#     Given a string, it splits it into VCV groups
-#     """
-#     values = []
-
-#     pattern = "[aeiouh]*[bcdfgjklmnpqrstvwxyz][_,:\?!&\.]*[aeiouh]*|"
-#     pattern += "[aeiouh]*[bcdfgjklmnpqrstvwxyz_,:\?!&\.][aeiouh]*"
-
-#     pattern = compile(
-#         pattern=pattern,
-#         flags=MULTILINE,
-#     )
-
-#     word_index = 0
-#     for word in string.split(" "):
-
-#         for match in pattern.finditer(string=word):
-#             if match:
-#                 group = ""
-#                 index = 0
-
-#                 for i in range(*match.span(0)):
-#                     index = i + word_index
-#                     group += word[i]
-
-#                 word_index = index
-
-#                 values.append(
-#                     dict(
-#                         index=word_index,
-#                         group=group,
-#                     )
-#                 )
-
-#     values = sorted(
-#         values,
-#         key=lambda item: item.get("index"),
-#     )
-
-#     values = [item.get("group").strip() for item in values]
-
-#     if not len(values):
-#         raise ValueError('Words can not contain only vowels and/or "h"')
-
-#     return values
-
-
-# def populate_array(template: List[Union[int, List[int]]], base: List[str]) -> List[int]:
-#     """
-#     Replicates a template array as many times as needed
-#     """
-#     out = template * ((len(base) // len(template)) + 1)
-#     return out[: len(base)]
-
-
-# def compute_group_angles(groups: List[str]) -> List[int]:
-#     """
-#     Decides how much to rotate a group
-#     """
-#     angles = populate_array(
-#         template=[1, 3, 2, 0],
-#         base=groups,
-#     )
-#     return [0 if i == 0 else a for i, a in enumerate(angles)]
-
-
-# def is_vowel(character: str) -> bool:
-#     """
-#     Decides whether a character is a vowel or not
-#     """
-#     return character in "aeiouh&"
-
-
-# def remove_vowels(string: str) -> str:
-#     """
-#     Removes vowels from a group
-#     """
-#     return "".join([character for character in string if not is_vowel(character)])
-
-
-# def compute_start_caps(groups: List[str], string: str) -> List[Optional[int]]:
-#     """
-#     Decides whether a start cap is needed
-#     """
-#     angles = populate_array(
-#         template=[2, 3, 2, 1],
-#         base=groups,
-#     )
-#     checks = []
-#     string = remove_vowels(string)
-
-#     for i, group in enumerate(groups):
-#         group = remove_vowels(group)
-
-#         is_start = i == 0
-#         after_space = string[string.index(group) - 1] == " "
-
-#         checks.append(is_start or after_space)
-#         string = string[len(group) :]
-
-#     return [0 if i == 0 else a if checks[i] else None for i, a in enumerate(angles)]
-
-
-# def compute_end_caps(groups: List[str], string: str) -> List[Optional[int]]:
-#     """
-#     Decides whether an end cap is needed
-#     """
-#     angles = populate_array(
-#         template=[1, 0, 3, 0],
-#         base=groups,
-#     )
-#     checks = []
-#     string = remove_vowels(string)
-
-#     for i, group in enumerate(groups):
-#         group = remove_vowels(group)
-
-#         is_end = i == len(groups) - 1
-#         try:
-#             before_space = string[string.index(group) + len(group)] == " "
-#         except IndexError:
-#             pass
-
-#         checks.append(is_end or before_space)
-#         string = string[len(group) :]
-
-#     return [
-#         1 if i == 0 and checks[i] else angle if checks[i] else None
-#         for i, angle in enumerate(angles)
-#     ]
-
-
-# def compute_start_connectors(groups: List[str], string: str) -> List[Optional[int]]:
-#     """
-#     Decides whether a start connector is needed
-#     """
-#     angles = populate_array(
-#         template=[2, 3, 2, 1],
-#         base=groups,
-#     )
-#     checks = []
-#     string = remove_vowels(string)
-
-#     for i, group in enumerate(groups):
-#         group = remove_vowels(group)
-
-#         not_start = i != 0
-#         not_before_space = string[string.index(group) - 1] != " "
-
-#         checks.append(not_start and not_before_space)
-#         string = string[len(group) :]
-
-#     return [a if checks[i] else None for i, a in enumerate(angles)]
-
-
-# def compute_end_connectors(groups: List[str], string: str) -> List[Optional[int]]:
-#     angles = populate_array(
-#         template=[1, 0, 3, 0],
-#         base=groups,
-#     )
-#     checks = []
-#     string = remove_vowels(string)
-
-#     for i, group in enumerate(groups):
-#         group = remove_vowels(group)
-
-#         before_end = i != len(groups) - 1
-#         try:
-#             not_before_space = string[string.index(group) + len(group)] != " "
-#         except IndexError:
-#             pass
-
-#         checks.append(before_end and not_before_space)
-#         string = string[len(group) :]
-
-#     return [
-#         1 if i == 0 and checks[i] else a if checks[i] else None
-#         for i, a in enumerate(angles)
-#     ]
-
-
-# def compute_group_horizontals(groups: List[str]) -> List[int]:
-#     return populate_array(
-#         template=[0, 1, 1, 0],
-#         base=groups,
-#     )
-
-
-# def compute_group_verticals(groups: List[str]) -> List[int]:
-#     return [index // 2 for index in range(len(groups))]
-
-
-# def compute_vowel_locations(
-#     groups: List[List[str]],
-#     horizontals: List[int],
-#     verticals: List[int],
-#     angles: List[int],
-# ) -> List[dict[str, Union[str, int]]]:
-#     columns_template = [
-#         [0, 1],
-#         [0, 1],
-#         [2, 1],
-#         [1, 2],
-#     ]
-#     out = []
-
-#     for group_index, group in enumerate(groups):
-#         consonant_index = [is_vowel(c) for c in group].index(False)
-
-#         for i, character in enumerate(group):
-#             if is_vowel(character):
-#                 angle = columns_template[angles[group_index]]
-
-#                 horizontal = angle[int(i > consonant_index)]
-#                 vertical = verticals[group_index]
-
-#                 vertical_offset = 1 if horizontal in [0, 2] else 0
-
-#                 current = dict(
-#                     character=character,
-#                     horizontal=horizontals[group_index],
-#                     vertical=vertical,
-#                     horizontal_offset=horizontal,
-#                     vertical_offset=vertical_offset,
-#                 )
-
-#                 out.append(current)
-
-#     for i in range(1, len(out)):
-#         prev = out[i - 1]
-#         current = out[i]
-
-#         if (
-#             prev.get("character") != "h"
-#             and current.get("horizontal") == prev.get("horizontal")
-#             and current.get("vertical") == prev.get("vertical")
-#         ):
-#             out[i]["vertical_offset"] += prev.get("vertical_offset") + 1
-
-#     return out
-
-
 import re
 from typing import Any, List, Optional, Union
 from PIL import Image
@@ -521,12 +263,15 @@ class Diabolic:
                 if not group.get("positions").get("vowels")[i]
             ].pop()
 
+            h = group.get("column") * self.SIZE
+            v = group.get("row") * self.SIZE
+
             self.base_image = place_character(
                 char=consonant,
                 angle=group.get("angle"),
                 base=self.base_image,
-                horizontal=group.get("column") * self.SIZE,
-                vertical=group.get("row") * self.SIZE,
+                horizontal=h,
+                vertical=v,
             )
 
             if group.get("start_cap") is not None:
@@ -534,8 +279,8 @@ class Diabolic:
                     char="cap",
                     angle=group.get("start_cap"),
                     base=self.base_image,
-                    horizontal=group.get("column") * self.SIZE,
-                    vertical=group.get("row") * self.SIZE,
+                    horizontal=h,
+                    vertical=v,
                 )
 
             if group.get("end_cap") is not None:
@@ -543,8 +288,8 @@ class Diabolic:
                     char="cap",
                     angle=group.get("end_cap"),
                     base=self.base_image,
-                    horizontal=group.get("column") * self.SIZE,
-                    vertical=group.get("row") * self.SIZE,
+                    horizontal=h,
+                    vertical=v,
                 )
 
             if group.get("start_connect") is not None:
@@ -552,8 +297,8 @@ class Diabolic:
                     char="connect",
                     angle=group.get("start_connect"),
                     base=self.base_image,
-                    horizontal=group.get("column") * self.SIZE,
-                    vertical=group.get("row") * self.SIZE,
+                    horizontal=h,
+                    vertical=v,
                 )
 
             if group.get("end_connect") is not None:
@@ -562,8 +307,16 @@ class Diabolic:
                     angle=group.get("end_connect"),
                     base=self.base_image,
                     horizontal=group.get("column") * self.SIZE,
-                    vertical=group.get("row") * self.SIZE,
+                    vertical=v,
                 )
+
+            # index = 0
+            # for i, c in enumerate(characters):
+            #     if group.get("positions").get("vowels")[i]:
+            #         for before in group.get("positions").get("before"):
+            #             print(before)
+
+            # # print(group.get("string"), group.get("positions"))
 
     def show(self) -> None:
         self.base_image.show()
@@ -595,5 +348,4 @@ if __name__ == "__main__":
 
     for string in strings:
         s = Diabolic(string)
-        print(s.__dict__)
         s.show()
